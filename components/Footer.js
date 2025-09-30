b743695b7ca0479738f840afd52558a6f7d35611
axirewards <axirewards@gmail.com>
2025-09-30 21:26:58 +0200

    Update footer copyright message

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-primary text-white py-4 mt-6">
       <div className="container mx-auto text-center">
-        &copy; {new Date().getFullYear()} AxiRewards. Visi teisės saugomos.
+        &copy; {new Date().getFullYear()} AxiRewards. All rights reserved.
       </div>
     </footer>
   )
