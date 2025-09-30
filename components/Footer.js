9fa094fbc23644f4eaa212232c5fe5589071bdf6
axirewards <axirewards@gmail.com>
2025-09-30 16:01:33 +0300

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-primary text-white py-4 mt-6">
+      <div className="container mx-auto text-center">
+        &copy; {new Date().getFullYear()} AxiRewards. Visi teisės saugomos.
+      </div>
+    </footer>
+  )
+}
