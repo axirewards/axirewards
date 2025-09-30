f281a1d318966bb2d4b4aaea9810de7b5070d94c
axirewards <axirewards@gmail.com>
2025-09-30 15:15:50 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF', // navy blue
+        accent: '#3B82F6', // light blue accent
+        bg: '#F9FAFB', // background
+        card: '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+}
